e2e70acfd28b005d0237a41c3dd4032de3c7170d
Sarath235 <147380391+Sarath235@users.noreply.github.com>
2023-10-09 13:41:03 +0530

    Update Sentimental analysis.py

--- Sentimental analysis.py
+++ Sentimental analysis.py
@@ -1,1 +1,17 @@
+from textblob import TextBlob
 
+def analyze_sentiment(text):
+    analysis = TextBlob(text)
+    
+    # Classify the polarity of the text
+    if analysis.sentiment.polarity > 0:
+        return 'Positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'Neutral'
+    else:
+        return 'Negative'
+
+# Example usage
+marketing_text = "Our new product is innovative and amazing!"
+sentiment = analyze_sentiment(marketing_text)
+print(f"Sentiment: {sentiment}")
